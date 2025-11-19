4e873a7f143f4b8e442c8d36cdcce3bdd6ab49c0
miguelonCDS <miguel264gomez@gmail.com>
2025-11-19 12:39:56 -0700

    perfil usuario puede ver sus favoritos

--- php/database.php
+++ php/database.php
@@ -1,19 +1,15 @@
 <?php
-// Configuración de la conexión a la base de datos local
-$host = "localhost";
-$usuario = "root";
-$contrasena = "Ramirez034";
+$host = "31.220.96.192";
+$usuario = "admin_comercial";
+$contrasena = "ComerzialMC@12";
 $baseDeDatos = "comercializadora";
 $puerto = 3306;
 
-// Establecer la conexión usando el procedimiento de MySQLi
 $conexion = mysqli_connect($host, $usuario, $contrasena, $baseDeDatos, $puerto);
 
-// Verificar si la conexión fue exitosa
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 }
 
-// Establecer el juego de caracteres a UTF-8 para evitar problemas con tildes y caracteres especiales
 mysqli_set_charset($conexion, "utf8mb4");
 ?>
