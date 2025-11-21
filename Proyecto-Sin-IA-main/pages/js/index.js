3289d8a62f75ca9b58b57aeb81a9cd0f4d47eb23
Janai034 <kominomisenpai098@gmail.com>
2025-11-20 18:20:29 -0700

    Delete Proyecto-Sin-IA-main directory

--- Proyecto-Sin-IA-main/pages/js/index.js
+++
@@ -1,9 +0,0 @@
-function agregarAlCarrito(productoId) {
-    const informacion = new FormData();
-    informacion.append('producto_id', productoId);
-    
-    fetch('pages/agregar_carrito.php', {
-        method: 'POST',
-        body: informacion
-    })
-}
